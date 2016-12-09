b581d31095aee517dea462fb34464fbb248b8311
Douglas <douglastran2011@gmail.com>
2016-12-09 08:45:07 -0500

    finaly done
    ASDHDIFDSHIGDIBHE

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
+var myWindow;
+function openWin1() {
+    myWindow = window.open("images/Origami/kusu1.jpg","","width=auto, height=auto");
+}
 
-  $('div').on('click', function() {
-      $(this).toggleClass('show-description');
-  });
-
-
-
-
-
-$(".imageclick").on('click', function() {
-	$(this).toggleClass('enlarge');
-});
+function openWin2() {
+    myWindow = window.open("images/Origami/kusu2.jpg","","width=auto, height=auto");
+}
+function openWin3() {
+    myWindow = window.open("images/Origami/kusu3.jpg","","width=auto, height=auto");
+}
+function openWin4() {
+    myWindow = window.open("images/Origami/kusu4.jpg","","width=auto, height=auto");
+}
+function openWin5() {
+    myWindow = window.open("images/Origami/kusu5.jpg","","width=auto, height=auto");
+}
+function openWin6() {
+    myWindow = window.open("images/Origami/kusu6.jpg","","width=auto, height=auto");
+}
+function openWin7() {
+    myWindow = window.open("images/Origami/tetralamp.jpg","","width=auto, height=auto");
+}
+function openWin8() {
+    myWindow = window.open("images/Origami/kinetic.jpg","","width=auto, height=auto");
+}
